86acd40c4e2dd3a33feeacded614ecffb2452688
Lee Cottrell <44240965+leecottrellPTC@users.noreply.github.com>
2023-03-06 12:09:30 -0500

    yet again

---
+++ azurehotel/src/test/java/com/leecottrell/azurehotel/AzurehotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leecottrell.azurehotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzurehotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
